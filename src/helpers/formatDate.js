e7ca1e673885d3f8656bed33dd59cf18ef7ed9fd
Austin Malmberg <austin.malmberg@gmail.com>
2019-11-27 12:19:21 -0500

    Fixed display bug at noon; Left padded some variables

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,4 +1,4 @@
-export default function formatDate(datetime) {
+function formatDate(datetime) {
 
   if (!datetime)
     return datetime;
@@ -6,9 +6,24 @@ export default function formatDate(datetime) {
   const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const date = new Date(datetime);
 
-  const [ hour, minute, half ] = [ date.getHours() % 12, date.getMinutes(), date.getHours() < 12 ? 'AM' : 'PM'];
-  const [ month, day, year ] = [ monthNames[date.getMonth()], date.getDate(), date.getFullYear() ]
+  const hour = padStart(date.getHours() <= 12 ? date.getHours() : date.getHours() - 12);
+  const minute = padStart(date.getMinutes());
+  const ampm = (date.getHours() < 12 ? 'AM' : 'PM');
 
-  return `${month} ${day}, ${year} at ${hour}:${minute} ${half}`;
+  const month = monthNames[date.getMonth()];
+  const day = padStart(date.getDate());
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year} at ${hour}:${minute} ${ampm}`;
 
 }
+
+function padStart(val) {
+
+  if (typeof val === 'string')
+    return val.padStart(2, '0');
+
+  return val.toString().padStart(2, '0');
+}
+
+export default formatDate;
